29473d30ef8c12d65766ce3ed2fbaf8564b30978
pocketberserker <pocketberserker@gmail.com>
2014-04-19 23:30:12 +0900

    implement binary parser

---
+++ FsAttoparsec.Tests/BinaryParserTest.fs
@@ -0,0 +1,100 @@
+﻿namespace Attoparsec.Tests
+
+open NUnit.Framework
+open FsUnit
+open FsCheck
+open FsCheck.NUnit
+open System
+open Attoparsec
+open Attoparsec.Binary
+
+[<TestFixture>]
+module BinaryParserTest =
+
+  let cons b xs = Array.append [| b |] xs
+
+  [<Test>]
+  let ``satisfy`` () =
+    check <| fun b xs ->
+      let actual =
+        cons b xs
+        |> parse (satisfy (fun x -> x <= b))
+        |> ParseResult.option
+      actual = Some b
+
+  [<Test>]
+  let ``byte`` () =
+    check <| fun b xs ->
+      let actual =
+        cons b xs
+        |> parse (byte_ b)
+        |> ParseResult.option
+      actual = Some b
+
+  [<Test>]
+  let ``anyByte`` () =
+    check <| fun xs ->
+      let p = (parse anyByte xs).Option
+      match List.ofArray xs with | [] -> p = None | x::_ -> p = Some x
+
+  [<Test>]
+  let ``notByte`` () =
+    check <| fun b xs ->
+      ((not <| Array.isEmpty xs) ==>
+        lazy (let v = xs.[0] in (parse (notByte b) xs).Option = (if v = b then None else Some v)))
+
+  [<Test>]
+  let ``bytes`` () =
+    check <| fun s t ->
+      (parse (bytes s) (Array.append s t)).Option = Some s
+
+  [<Test>]
+  let ``takeCount`` () =
+    check <| fun k s ->
+      (k >= 0) ==> lazy (match (parse (take k) s).Option with | None -> k > Array.length s | Some _ -> k <= Array.length s)
+
+  module Array =
+
+    let span pred (b: byte []) =
+      let t = b |> Seq.takeWhile pred |> Seq.toList
+      let u = b |> Seq.skipWhile pred |> Seq.toArray
+      (t, u)
+
+  [<Test>]
+  let ``takeWhile`` () =
+    check <| fun b xs ->
+      let (h, t) = Array.span ((=) b) xs
+      let h = Array.ofList h
+      xs
+      |> parseOnly (parser {
+        let! hp = takeWhile ((=) b)
+        let! tp = takeText
+        return (hp, tp)
+      })
+      |> ((=) (Choice1Of2 (h, t)))
+
+  [<Test>]
+  let ``takeWhile1`` () =
+    check <| fun b xs ->
+      let sp = cons b xs
+      let (h, t) = Array.span (fun x -> x <= b) sp
+      let h = Array.ofList h
+      sp
+      |> parseOnly (parser {
+        let! hp = takeWhile1 (fun x -> x <= b)
+        let! tp = takeText
+        return (hp, tp)
+      })
+      |> ((=) (Choice1Of2 (h, t)))
+
+  [<Test>]
+  let ``takeWhile1 empty`` () =
+    [||]
+    |> parse (Binary.takeWhile1 (fun _ -> true))
+    |> ParseResult.option
+    |> should equal None
+
+  [<Test>]
+  let ``endOfInput`` () =
+    check <| fun s ->
+      s |> parseOnly endOfInput = (if Array.isEmpty s then Choice1Of2 () else Choice2Of2 "endOfInput")
---
+++ FsAttoparsec/BinaryParser.fs
@@ -0,0 +1,64 @@
+﻿namespace Attoparsec
+
+module Binary =
+
+  let private monoid = { new Monoid<byte []>() with
+    member this.Mempty = [||]
+    member this.Mappend(s, t) = Array.append s t
+  }
+
+  module ParseResult =
+    let feed s (result: ParseResult<_, _>) = ParseResult.feed monoid s result
+    let done_ (result: ParseResult<_, _>) = ParseResult.done_ monoid result
+
+  let parseOnly parser input = parseOnly monoid parser input
+
+  let ensure n = ensure Array.length n
+
+  let inline private length s = Array.length s
+  let inline private head (b: byte []) = b.[0]
+  let inline private tail (b: byte []) = b |> Seq.skip 1 |> Seq.toArray
+  let private splitAt n (b: byte []) =
+    (b |> Seq.take n |> Seq.toArray, b |> Seq.skip n |> Seq.toArray)
+
+  let elem p what = elem length head tail p what
+
+  let satisfy p = satisfy length head tail p
+
+  let skip p what = skip length head tail p what
+
+  let private dropWhile p (b: byte []) = b |> Seq.skipWhile p |> Seq.toArray
+
+  let skipWhile p = skipWhile monoid dropWhile p
+
+  let takeWith n p what = takeWith length splitAt n p what
+
+  let take n = take length splitAt n
+
+  let anyByte: Parser<byte [], byte> = satisfy (fun _ -> true)
+
+  let notByte c = (satisfy ((<>) c)).As("not '" + (string c) + "'")
+
+  let private span pred (b: byte []) =
+    let t = b |> Seq.takeWhile pred |> Seq.toList
+    let u = b |> Seq.skipWhile pred |> Seq.toArray
+    (t, u)
+
+  let takeWhile (p: _ -> bool) : Parser<byte [], byte []> =
+    takeWhile monoid span p |> map List.toArray
+
+  let takeRest = takeRest monoid
+
+  let takeText = takeText monoid List.fold
+
+  let byte_ c = elem ((=) c) (Some ("'" + (string c) + "'"))
+  let bytes (b: byte []) = takeWith (Array.length b) ((=) b) (Some (b.ToString()))
+
+  let takeWhile1 p : Parser<byte [], byte[]> =
+    takeWhile1 monoid span p |> map List.toArray
+
+  let scan s p = scan monoid head tail (fun n (x: byte []) -> x |> Seq.skip n |> Seq.toArray) s p
+
+  let parse (m: Parser<byte [], _>) init = m.Parse(monoid, init)
+
+  let parseAll m init = parse (phrase m) init
