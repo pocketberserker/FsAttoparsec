f0ac7d0d9abde64115420b170ff8083f049b5687
y.nakayama <y.nakayama@otr-jp.com>
2014-04-24 14:18:02 +0900

    add inClass function

--- FsAttoparsec/Helper.fs
+++ FsAttoparsec/Helper.fs
@@ -5,3 +5,12 @@ module Helper =
   open System.Text.RegularExpressions
 
   let isMatch regex item = Regex.IsMatch(item, regex)
+
+  let private charClass s =
+    let rec inner = function
+      | a :: '-' :: b :: xs -> List.append [a..b] (inner xs)
+      | x :: xs -> x :: inner xs
+      | _ -> []
+    inner (List.ofSeq s) |> Seq.distinct
+
+  let inClass (s: string) c = charClass s |> Seq.exists ((=) c)
