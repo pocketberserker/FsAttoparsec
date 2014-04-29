2ce134194cc29c3edec5e98e9bce7241e06c61a1
pocketberserker <pocketberserker@gmail.com>
2014-04-29 23:36:41 +0900

    create Array module

--- FsAttoparsec/BinaryParser.fs
+++ FsAttoparsec/BinaryParser.fs
@@ -2,6 +2,38 @@
 
 open System.Diagnostics.Contracts
 
+module private Array =
+
+  let head (b: byte []) =
+    Contract.Requires(b.Length >= 1)
+    b.[0]
+
+  let tail (b: byte []) =
+    Contract.Requires(b.Length >= 1)
+    if Array.length b = 1 then [||]
+    else b.[ 1 .. ]
+
+  let splitAt n (b: byte []) =
+    Contract.Requires(b.Length >= 0)
+    if Array.isEmpty b then ([||], [||])
+    elif n <= 0 then ([||], b)
+    elif n >= b.Length then (b, [||])
+    else (b.[ .. n - 1 ], b.[ n .. ])
+  
+  let skipWhile p (b: byte []) =
+    match Array.tryFindIndex p b with
+    | Some i -> splitAt i b |> snd
+    | None -> [||]
+
+  let split pred (b: byte []) =
+    match Array.tryFindIndex pred b with
+    | Some i -> splitAt i b
+    | None -> (b, [||])
+
+  let span pred (b: byte []) = split (not << pred) b
+
+  let take n b = b |> splitAt n |> fst
+
 module Binary =
 
   let private monoid = { new Monoid<byte []>() with
@@ -17,51 +49,24 @@ module Binary =
 
   let ensure n = ensure Array.length n
 
-  let inline private length s = Array.length s
-  let inline private head (b: byte []) =
-    Contract.Requires(b.Length >= 1)
-    b.[0]
-  let inline private tail (b: byte []) =
-    Contract.Requires(b.Length >= 1)
-    if Array.length b = 1 then [||]
-    else b.[ 1 .. ]
-  let private splitAt n (b: byte []) =
-    Contract.Requires(b.Length >= 0)
-    if Array.isEmpty b then ([||], [||])
-    elif n <= 0 then ([||], b)
-    elif n >= b.Length then (b, [||])
-    else (b.[ .. n - 1 ], b.[ n .. ])
+  let elem p what = elem Array.length Array.head Array.tail p what
 
-  let elem p what = elem length head tail p what
+  let satisfy p = satisfy Array.length Array.head Array.tail p
 
-  let satisfy p = satisfy length head tail p
+  let skip p what = skip Array.length Array.head Array.tail p what
 
-  let skip p what = skip length head tail p what
+  let skipWhile p = skipWhile monoid Array.skipWhile p
 
-  let private dropWhile p (b: byte []) =
-    match Array.tryFindIndex p b with
-    | Some i -> splitAt i b |> snd
-    | None -> [||]
+  let takeWith n p what = takeWith Array.length Array.splitAt n p what
 
-  let skipWhile p = skipWhile monoid dropWhile p
-
-  let takeWith n p what = takeWith length splitAt n p what
-
-  let take n = take length splitAt n
+  let take n = take Array.length Array.splitAt n
 
   let anyByte: Parser<byte [], byte> = satisfy (fun _ -> true)
 
   let notByte c = (satisfy ((<>) c)).As("not '" + (string c) + "'")
 
-  let private split pred (b: byte []) =
-    match Array.tryFindIndex pred b with
-    | Some i -> splitAt i b
-    | None -> (b, [||])
-
-  let private span pred (b: byte []) = split (not << pred) b
-
   let takeWhile (p: _ -> bool) : Parser<byte [], byte []> =
-    takeWhile monoid span p
+    takeWhile monoid Array.span p
 
   let takeRest = takeRest monoid
 
@@ -71,10 +76,10 @@ module Binary =
   let bytes (b: byte []) = takeWith (Array.length b) ((=) b) (Some (b.ToString()))
 
   let takeWhile1 p : Parser<byte [], byte[]> =
-    takeWhile1 monoid span p
+    takeWhile1 monoid Array.span p
 
-  let scan s p = scan monoid head tail (fun n (x: byte []) -> x |> splitAt n |> fst) s p
-
+  let scan s p = scan monoid Array.head Array.tail Array.take s p
+  
   let parse (m: Parser<byte [], _>) init = m.Parse(monoid, init)
 
   let parseAll m init = parse (phrase m) init
