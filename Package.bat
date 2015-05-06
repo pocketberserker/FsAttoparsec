e1c21f4f9d4faebcf384a4f0da7f65372993458f
pocketberserker <pocketberserker@gmail.com>
2015-05-07 08:08:11 +0900

    fix package scirpt and configuration

--- Package.bat
+++
@@ -1,1 +0,0 @@
-.nuget\nuget.exe pack FsAttoparsec\FsAttoparsec.fsproj -Symbols -Build -Prop Configuration=Release -Properties VisualStudioVersion=12.0
---
+++ Packages.ps1
@@ -0,0 +1,4 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe FsAttoparsec.sln /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:rebuild
+
+.\.nuget\nuget.exe pack .\FsAttoparsec\FsAttoparsec.fsproj -Symbols -Properties VisualStudioVersion=12.0
+
